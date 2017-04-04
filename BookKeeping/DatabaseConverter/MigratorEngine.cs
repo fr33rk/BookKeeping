83faf46611d12ab03705af171855242be0a231a7
Freerk de Leeuw <fleeuw@hotmail.com>
2017-04-04 21:30:57 +0200

    Started with the database converter...

---
+++ BookKeeping/DatabaseConverter/MigratorEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PL.BookKeeping.Entities;
+
+namespace DatabaseConverter
+{
+    public class MigratorEngine
+    {
+	    public void ExportToFile()
+	    {
+		    var output = new StringBuilder();
+
+
+
+
+		    var testUser = new User() {CreationDT = DateTime.Now, Key = 1, Name = "Freerk"};
+
+		    var testOutput = JsonConvert.SerializeObject(testUser);
+	    }
+    }
+}
