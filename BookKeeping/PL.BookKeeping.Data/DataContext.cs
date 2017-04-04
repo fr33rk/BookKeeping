365409c10e37bfb9cf68d1ec8854de0c449daba3
Freerk de Leeuw <fleeuw@hotmail.com>
2017-04-04 19:42:38 +0200

    Made connection with MariaDB on Helium

--- BookKeeping/PL.BookKeeping.Data/DataContext.cs
+++ BookKeeping/PL.BookKeeping.Data/DataContext.cs
@@ -1,32 +1,34 @@
 ﻿using FirebirdSql.Data.FirebirdClient;
 using PL.BookKeeping.Entities;
 using System.Data.Entity;
+using MySql.Data.MySqlClient;
+using MySql.Data.Entity;
 
 namespace PL.BookKeeping.Data
 {
-    public class DataContext : DbContext
-    {
-        /// <summary>Gets or sets the current user.</summary>
-        /// <value>The current user.</value>
-        public User CurrentUser { get; set; }
+	[DbConfigurationType(typeof(MySqlEFConfiguration))]
+	public class DataContext : DbContext
+	{
+		/// <summary>Gets or sets the current user.</summary>
+		/// <value>The current user.</value>
+		public User CurrentUser { get; set; }
 
-        public DataContext()
-            //: base(new FbConnection(@"database=localhost:D:\Ontwikkeling\Eigen\BookKeeping\BookKeeping\Database\BookKeeping.fdb;user=sysdba;password=masterkey"), true)
-            : base(new FbConnection(@"database=localhost:D:\Projects\TSF\BookKeeping\BookKeeping\Database\BookKeeping.fdb;user=sysdba;password=masterkey"), true)
-        {
-            this.Configuration.LazyLoadingEnabled = false;
-        }
+		public DataContext()
+			: base(new MySqlConnection(@"Server=Helium;Database=Bookkeeping;Uid=Bookkeeper;Password=books"), true)
+		{
+			this.Configuration.LazyLoadingEnabled = false;
+		}
 
-        public DbSet<User> Users { get; set; }
+		public DbSet<User> Users { get; set; }
 
-        public DbSet<Transaction> Transactions { get; set; }
+		public DbSet<Transaction> Transactions { get; set; }
 
-        public DbSet<Entry> Entries { get; set; }
+		public DbSet<Entry> Entries { get; set; }
 
-        public DbSet<Period> Periods { get; set; }
+		public DbSet<Period> Periods { get; set; }
 
-        public DbSet<EntryPeriod> EntryPeriods { get; set; }
+		public DbSet<EntryPeriod> EntryPeriods { get; set; }
 
-        public DbSet<ProcessingRule> ProcessingRules { get; set; }
-    }
+		public DbSet<ProcessingRule> ProcessingRules { get; set; }
+	}
 }
