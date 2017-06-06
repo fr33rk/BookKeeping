c2a13def8a672672860851991294014d0984a80c
Freerk <Freerk@DESKTOP-G5UOM0V>
2017-06-06 20:44:11 +0200

    v2.0 MySql version

--- BookKeeping/BookKeeping.Client/Properties/AssemblyInfo.cs
+++ BookKeeping/BookKeeping.Client/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 [assembly: AssemblyTitle("BookKeeping.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pantera Leo")]
 [assembly: AssemblyProduct("BookKeeping.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -47,5 +47,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
